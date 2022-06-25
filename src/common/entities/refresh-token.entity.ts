7dc78ecb193f98b95fc6f9b58f254d77145d8f57
Grapeoff <grapeoff.official@gmail.com>
2022-06-25 23:48:00 +0300

    Added corresponding decorators

--- src/common/entities/refresh-token.entity.ts
+++ src/common/entities/refresh-token.entity.ts
@@ -1,5 +1,8 @@
 import { BasicEntity } from '@common/entities/basic.entity';
+import { Column, Entity } from 'typeorm';
 
+@Entity('refresh_tokens')
 export class RefreshTokenEntity extends BasicEntity {
+    @Column({ unique: true })
     owner_id: string;
 }
